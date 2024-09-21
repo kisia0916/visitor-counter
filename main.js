2617846c2e4da5668ea9b67633babe01dd8e9eba
kisia0916 <fumiharuabe@gmail.com>
2024-09-21 10:30:12 +0900

    degub

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const visNum = document.querySelector(".visitorNum")
 setInterval(()=>{
-    axios.post("http://localhost:3000/visitor/get-visitor",{
+    axios.post("http://172.16.202.101:80/visitor/get-visitor",{
         title:"main"
     },{auth:{
         username:"fumi20080916",
         password:20080916
     }}).then((res)=>{
-        visNum.textContent = res.data.data.counter
+        visNum.textContent = res.data.data.counter + "人"
     })
 },30000)
